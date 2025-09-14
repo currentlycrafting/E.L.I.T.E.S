f9869f8081d56a2cf93916efe6a02b20294a6aaf
salman <152821065+currentlycrafting@users.noreply.github.com>
2025-09-14 12:07:07 -0500

    Initial Commit

---
+++ ELITES/ELITESApp.swift
@@ -0,0 +1,17 @@
+//
+//  ELITESApp.swift
+//  ELITES
+//
+//  Created by Someone Guy on 9/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ELITESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ELITESTests/ELITESTests.swift
@@ -0,0 +1,17 @@
+//
+//  ELITESTests.swift
+//  ELITESTests
+//
+//  Created by Someone Guy on 9/14/25.
+//
+
+import Testing
+@testable import ELITES
+
+struct ELITESTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
